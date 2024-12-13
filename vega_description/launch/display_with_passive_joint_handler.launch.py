51901876dbe0c96ebbdd601343a2cdfae6753f4a
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-12-13 23:22:19 +0100

    Added passive joint handler

---
+++ vega_description/launch/display_with_passive_joint_handler.launch.py
@@ -0,0 +1,66 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue  # Import ParameterValue
+
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    urdf_file = 'vega_description.urdf'
+    package_description = "vega_description"
+
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{
+            'use_sim_time': True,
+            'robot_description': ParameterValue(Command(['xacro ', robot_desc_path]), value_type=str)
+        }],
+        output="screen"
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        parameters=[{
+            'rate': 200,
+            'source_list': ['passive_joint_states']
+        }],
+        output='screen',
+    )
+
+    passive_joint_handler_node = Node(
+        package=package_description,
+        executable='passive_joints.py',
+        output = 'screen',
+    )
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir]
+    )
+
+    # create and return launch description object
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        passive_joint_handler_node,
+        rviz_node,
+    ])
+
