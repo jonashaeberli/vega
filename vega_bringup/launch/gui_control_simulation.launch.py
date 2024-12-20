69ad453f417916866b46850e15716fc981236eca
HDJGJonas <jonas.haeberli@hispeed.ch>
2024-12-20 21:45:25 +0100

    changes

---
+++ vega_bringup/launch/gui_control_simulation.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # Get the path to the launch file in package_a
+    vega_description_launch_dir = os.path.join(get_package_share_directory('vega_description'), 'launch')
+    launch_file_path = os.path.join(vega_description_launch_dir, 'display_simulation.launch.py')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(launch_file_path),
+        )
+    ])
